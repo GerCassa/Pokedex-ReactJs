8961cd7c58a7a4c31d2fbaca3b674a90403ba6bd
GerCassa <gercassa@gmail.com>
2019-08-30 18:08:33 +0100

    git ignore added

---
+++ build/precache-manifest.0d3a8b934e852e75175e1153351f2db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/static/media/Pokemon Hollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "7e98bfcef67ba7635d02ca100e592336",
+    "url": "/static/media/Pocket Monk.7e98bfce.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2383ae46e6cb865e8bf0",
+    "url": "/static/js/main.2383ae46.chunk.js"
+  },
+  {
+    "revision": "424c0f7d30331a79098a",
+    "url": "/static/js/1.424c0f7d.chunk.js"
+  },
+  {
+    "revision": "2383ae46e6cb865e8bf0",
+    "url": "/static/css/main.69c81b69.chunk.css"
+  },
+  {
+    "revision": "cf4547eb83061b003e45393086928ad7",
+    "url": "/index.html"
+  }
+];
