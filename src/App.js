ec7372c54310e3a9e73a61eb012320cfd30eb001
GerCassa <gercassa@gmail.com>
2021-02-28 21:40:25 +0000

    container route modified in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import PokemonContainer from './Containers/PokemonContainer';
+import PokemonContainer from './Containers/PokemonContainer.jsx';
 
 class App extends Component {
   render() {
